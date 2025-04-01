b750bed227a2daf34a6988df3db96162db5496e6
Sandeep <sandipsharma2445@gmail.com>
2025-04-01 13:33:14 +0530

    UI created

---
+++ src/components/Counter.js
@@ -0,0 +1,13 @@
+import "./counter.css";
+
+const Counter = () => {
+    return (
+        <div className="card">
+            <p className="text">00</p>
+            <button>Increment</button>
+            <button>Decrement</button>
+        </div>
+    )
+}
+
+export default Counter;
